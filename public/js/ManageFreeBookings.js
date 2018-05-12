3ee25a41fbc7910f2bdbdb773bb32bad1c9afa36
EmadAlareefi <Emad.alareefi@gmail.com>
2018-05-12 17:34:23 +0300

    stable with update room

--- public/js/ManageFreeBookings.js
+++ public/js/ManageFreeBookings.js
@@ -15,11 +15,76 @@ var btnCreateCustomerClose = $("#btnCreateCustomerClose");
 //     frmAddingRoom.get(0).reset();
 //   }
 // })
-frmCreateCustomer.submit(() => {
-  btnCreateCustomerClose.click();
-  submit();
-  return false;
-});
+// frmCreateCustomer.submit(() => {
+//   btnCreateCustomerClose.click();
+//   submit();
+//   return false;
+// });
+
+(function($) {
+  "use strict";
+
+  /*==================================================================
+    [ Validate ]*/
+  var input = $(".validate-input .combobox");
+
+  $(".validate-form").on("submit", function() {
+    var check = true;
+
+    for (var i = 0; i < input.length; i++) {
+      if (customerValidate(input[i]) == false) {
+        showValidate(input[i]);
+        check = false;
+      }
+    }
+
+    return check;
+  });
+
+  $(".validate-form .combobox").each(function() {
+    $(this).focus(function() {
+      hideValidate(this);
+    });
+  });
+
+  function customerValidate(input) {
+    // if (
+    //   $(input).attr("type") == "firstName" ||
+    //   $(input).attr("name") == "firstName"
+    // ) {
+    //   if (
+    //     $(input)
+    //       .val()
+    //       .trim()
+    //       .match(
+    //         /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+    //       ) == null
+    //   ) {
+    //     return false;
+    //   }
+    // } else {
+      if (
+        $(input)
+          .val()
+          .trim() == ""
+      ) {
+        return false;
+      }
+    }
+  // }
+
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).addClass("alert-validate");
+  }
+
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).removeClass("alert-validate");
+  }
+})(jQuery);
 
 $(".formClose").on("click", () => {
   frmAddingRoom.get(0).reset();
--- routes/customers.js
+++ routes/customers.js
@@ -1,9 +1,12 @@
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
+
 var express = require("express");
 var router = express.Router();
 var MongoClient = require("mongodb").MongoClient;
 var globals = require("./globals");
 
-router.get("/",globals.ensureAuthenticated, (req, res, next) => {
+router.get("/", globals.ensureAuthenticated, (req, res, next) => {
   MongoClient.connect(globals.url, (err, db) => {
     if (err) {
       res.render("Pages/customers", {
@@ -44,75 +47,91 @@ router.get("/",globals.ensureAuthenticated, (req, res, next) => {
   });
 });
 
-router.post("/addCustomer", (req, res, next) => {
-  var body = req.body;
-  var firstName = firstName;
-  var secondName = body.secondName;
-  var thirdName = body.thirdName;
-  var lastName = body.lastName;
-  var customerType = body.customerType;
-  var nationality = body.nationality;
-  var idType = body.idType;
-  var cardCopyNum = body.cardCopyNum;
-  var idNum = body.idNum;
-  var issuingPlace = body.issuingPlace;
-  var expDate = body.expDate;
-  var phone = body.phone;
-  var workPhone = body.workPhone;
-  var email = body.email;
-  var category = body.category;
-  var address = body.address;
-  var specialNotes = body.specialNotes;
-  var notes = body.notes;
+router.post(
+  "/addCustomer",
+  [
+    check('firstName')
+    // Every validator method in the validator lib is available as a
+    // method in the check() APIs.
+    // You can customize per validator messages with .withMessage()
+    .equals("").withMessage("لا بد من اضافة الاسم الثاني"),
+    check('secondName',"لا بد من اضافة الاسم الثاني")
+    // Every validator method in the validator lib is available as a
+    // method in the check() APIs.
+    // You can customize per validator messages with .withMessage()
+    .exists(),
+  ]
+  ,
+  (req, res, next) => {
+    var body = req.body;
+    var firstName = body.firstName;
+    var secondName = body.secondName;
+    var thirdName = body.thirdName;
+    var lastName = body.lastName;
+    var customerType = body.customerType;
+    var nationality = body.nationality;
+    var idType = body.idType;
+    var cardCopyNum = body.cardCopyNum;
+    var idNum = body.idNum;
+    var issuingPlace = body.issuingPlace;
+    var expDate = body.expDate;
+    var phone = body.phone;
+    var workPhone = body.workPhone;
+    var email = body.email;
+    var category = body.category;
+    var address = body.address;
+    var specialNotes = body.specialNotes;
+    var notes = body.notes;
 
-  req.checkBody("firstName", "ﻻ ﺑﺪ ﻣﻦ اﺿﺎﻓﺔ اسم الغرفة").notEmpty();
-  req.checkBody("secondName", "لا بد من اضافة رقم الطابق").notEmpty();
-  req.checkBody("thirdName", "ﻻ ﺑﺪ ﻣﻦ اﺿﺎﻓﺔ اسم الغرفة").notEmpty();
-  req.checkBody("lastName", "لا بد من اضافة رقم الطابق").notEmpty();
-  req.checkBody("customerType", "لا بد من اختيار نوع العميل").equals("");
-  // req.checkBody("nationality", "لا بد من اضافة جنسية العميل").notEmpty();
-  // req.checkBody("roomNumber", "ﻻ ﺑﺪ ﻣﻦ اﺿﺎﻓﺔ اسم الغرفة").notEmpty();
-  // req.checkBody("floor", "لا بد من اضافة رقم الطابق").notEmpty();
+    // req.checkBody("firstName", "لا بد من اضافة الاسم الأول").notEmpty();
+    // req.checkBody("secondName", "لا بد من اضافة الاسم الثاني").notEmpty();
+    // req.checkBody("thirdName", "لا بد من اضافة الاسم الثالث").notEmpty();
+    // req.checkBody("lastName", "لا بد من اضافة الاسم اللقب").notEmpty();
+    // req.checkBody("customerType", "لا بد من اختيار نوع العميل").equals("اختر");
+    // req.checkBody("nationality", "لا بد من اضافة جنسية العميل").notEmpty();
+    // req.checkBody("roomNumber", "ﻻ ﺑﺪ ﻣﻦ اﺿﺎﻓﺔ اسم الغرفة").notEmpty();
+    // req.checkBody("floor", "لا بد من اضافة رقم الطابق").notEmpty();
 
-  var errors = req.validationErrors();
+    var errors = req.validationErrors();
 
-  if (errors) {
-    console.log(errors);
-    res.redirect("/ManageFreeBookings");
-  } else {
-    MongoClient.connect(globals.url, (err, db) => {
-      if (err) throw err;
-
-      dbo = db.db(globals.dbName);
-      var new_customer = {
-        firstName: firstName,
-        secondName: secondName,
-        thirdName: thirdName,
-        lastName: lastName,
-        customerType: customerType,
-        nationality: nationality,
-        idType: idType,
-        cardCopyNum: cardCopyNum,
-        idNum: idNum,
-        issuingPlace: issuingPlace,
-        expDate: expDate,
-        phone: phone,
-        workPhone: workPhone,
-        email: email,
-        category: category,
-        address: address,
-        specialNotes: specialNotes,
-        notes: notes
-      };
-
-      dbo.collection("customers").save(new_customer, (err, result) => {
+    if (errors) {
+      console.log(errors);
+      res.redirect("/ManageFreeBookings");
+    } else {
+      MongoClient.connect(globals.url, (err, db) => {
         if (err) throw err;
-        res.redirect("/ManageFreeBookings");
-        // res.end();
-        db.close();
+
+        dbo = db.db(globals.dbName);
+        var new_customer = {
+          firstName: firstName,
+          secondName: secondName,
+          thirdName: thirdName,
+          lastName: lastName,
+          customerType: customerType,
+          nationality: nationality,
+          idType: idType,
+          cardCopyNum: cardCopyNum,
+          idNum: idNum,
+          issuingPlace: issuingPlace,
+          expDate: expDate,
+          phone: phone,
+          workPhone: workPhone,
+          email: email,
+          category: category,
+          address: address,
+          specialNotes: specialNotes,
+          notes: notes
+        };
+
+        dbo.collection("customers").save(new_customer, (err, result) => {
+          if (err) throw err;
+          res.redirect("/ManageFreeBookings");
+          // res.end();
+          db.close();
+        });
       });
-    });
+    }
   }
-});
+);
 
 module.exports = router;
