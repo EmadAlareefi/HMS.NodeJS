c985bb452064308b5b3c066ad7d2f1f47fea3e95
AbdulrahmanShaiban <Aboodshi777@gmail.com>
2018-05-23 21:25:22 +0300

    a

---
+++ routes/globals.js
@@ -0,0 +1,19 @@
+
+
+// var url = 'mongodb://EmadAlareefi:emadalareefi@ds255588.mlab.com:55588/hmsdb'; 
+// var url = 'mongodb://localhost:27017/hmsdb'
+
+var Globals = {
+    url:'mongodb://localhost:27017/hmsdb',
+    dbName:"hmsdb",
+    ensureAuthenticated: function ensureAuthenticated(req, res, next){
+        if(req.isAuthenticated()){
+            return next();
+        } else {
+            //req.flash('error_msg','You are not logged in');
+            res.redirect('/login');
+        }
+    }
+}
+
+module.exports = Globals;
